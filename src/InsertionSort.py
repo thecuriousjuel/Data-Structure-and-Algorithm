0804178675858d941277b2316e27021be302b7df
Biswajit Basak <juelbasak@gmail.com>
2022-03-31 15:16:07 +0530

    Working on insertion sort

--- src/InsertionSort.py
+++ src/InsertionSort.py
@@ -1,15 +1,24 @@
-import this
-
-
 def insertion_sort(thislist):
     length_of_list = len(thislist)
-    for i in range(length_of_list):
-        for j in range(i+1, length_of_list):
-            if thislist[j]
+    for i in range(length_of_list-1):
+        
+        for j in range(i, 0, -1):
+            print(f'{thislist[i]} < {thislist[j]} -> {thislist[i] < thislist[j]}')
+            # if thislist[index]  < thislist[j]:
+            #     continue
+            
+
+            # temp = thislist[j]
+            # thislist[j] = thislist[index] 
+            # for k in range(j+1, length_of_list-1):
+            #     thislist[k] = thislist[k+1]
+        # print(thislist)
+    return thislist
 
 
 
-mylist = [22, 17, 86, 54, 99]
+
+mylist = [4,3,2,10,12,1,5,6]
 print('Original Data -> ', mylist)
 print('Sorted Data -> ', insertion_sort(mylist))
 
