d237291bd373310237298439b33a628415905b9a
Biswajit Basak <juelbasak@gmail.com>
2022-06-28 19:12:22 +0530

    Unique paths in grid done

--- Source Codes/Dynamic Programming/self_no_of_unique_paths_in_grid.py
+++ Source Codes/Dynamic Programming/self_no_of_unique_paths_in_grid.py
@@ -3,32 +3,31 @@
     
 """
 
-def fun(maze, row, col, path, ans):
+def fun(maze, row, col, path):
     
-    if row == len(maze):
-        return
-    
-    if col == len(maze[0]):
-        return
+    if row == len(maze) or col == len(maze[0]):
+        return 0
 
     path.append((row, col))
     
     if row == len(maze)-1 and col == len(maze[0])-1:
-        print('Destination reached. Path -> ', path)
-        ans[0] += 1
+        # print('Destination Reached. Path -> ', path)
+        path.pop()
+        return 1
     
-    fun(maze, row, col+1, path, ans)
-    fun(maze, row+1, col, path, ans)
+    right = fun(maze, row, col+1, path)
+    down = fun(maze, row+1, col, path)
     path.pop()
 
+    return right + down
 
-N = 4
-M = 4
+
+N = 15
+M = 15
 row, col = 0, 0
-ans = [0]
 path = []
 
 maze = [[1 for _ in range(M)] for _ in range(N)]
 
-fun(maze, row, col, path, ans)
-print(ans[0])
+out = fun(maze, row, col, path)
+print(out)
--- Source Codes/Dynamic Programming/self_no_of_unique_paths_in_grid_dp.py
+++ Source Codes/Dynamic Programming/self_no_of_unique_paths_in_grid_dp.py
@@ -1,34 +1,44 @@
 """
-    Program to find out the number of unique paths in a grid
+    Program to find out the number of unique paths in a grid using Dynamic Programming
     
 """
 
-def fun(maze, row, col, path, ans):
-    
-    if row == len(maze):
-        return
-    
-    if col == len(maze[0]):
-        return
+def fun(maze, row, col, path, dp):
+
+    if row == len(maze) or col == len(maze[0]):
+        return 0
 
     path.append((row, col))
+    # print(dp, (row, col))
     
     if row == len(maze)-1 and col == len(maze[0])-1:
-        print('Destination reached. Path -> ', path)
-        ans[0] += 1
-    
-    fun(maze, row, col+1, path, ans)
-    fun(maze, row+1, col, path, ans)
+        # print('Destination reached. Path -> ', path)
+
+        path.pop()
+        return 1
+
+    if dp[row][col] != 0:
+        return dp[row][col]
+
+    left = fun(maze, row, col+1, path, dp)
+
+    right = fun(maze, row+1, col, path, dp)
     path.pop()
 
+    dp[row][col] = left + right
 
-N = 4
-M = 4
+    return dp[row][col]
+
+
+N = 3
+M = 3
 row, col = 0, 0
-ans = [0]
 path = []
 
 maze = [[1 for _ in range(M)] for _ in range(N)]
+dp = [[0 for _ in range(M)] for _ in range(N)]
 
-fun(maze, row, col, path, ans)
-print(ans[0])
+fun(maze, row, col, path, dp)
+# print(dp)
+print(dp[0][0])
+
