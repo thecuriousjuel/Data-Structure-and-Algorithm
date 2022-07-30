0af0677c7aafd698cfe71de5736adfcd2e89f608
Biswajit Basak <juelbasak@gmail.com>
2022-07-30 14:30:50 +0530

    Not completed : Working on Shortest common supersequence

--- Source Codes/Dynamic Programming/DP on strings/shortest_common_supersequence_recursion.py
+++ Source Codes/Dynamic Programming/DP on strings/shortest_common_supersequence_recursion.py
@@ -1,15 +1,43 @@
-def fun(str1, str2, ind1, ind2):
+# Not completed
+
+
+def display(str1, str2, dp):
+    print('\t\t', end='')
+    for i in str2:
+        print(i, end='\t')
+    
+    print()
+    k = -1
+    for i in dp:
+        if k != -1:
+            print(str1[k], end='\t')
+        else:
+            print(end='\t')
+            
+        for j in i:
+            print(j, end='\t')
+        print()
+        
+        k+=1
+        
+
+def fun(str1, str2, ind1, ind2, dp):
     
     if ind1 < 0 or ind2 < 0:
         return 0
     
-    if str1[ind1] == str2[ind2]:
-        return 1 + fun(str1, str2, ind1-1, ind2-1)
-
-    left = fun(str1, str2, ind1, ind2-1)
-    right = fun(str1, str2, ind1-1, ind2)
+    if dp[ind1][ind2] != -1:
+        return dp[ind1][ind2]
     
-    return max(left, right)
+    if str1[ind1] == str2[ind2]:
+        return 1 + fun(str1, str2, ind1-1, ind2-1, dp)
+
+    left = fun(str1, str2, ind1, ind2-1, dp)
+    right = fun(str1, str2, ind1-1, ind2, dp)
+    
+    dp[ind1][ind2] = max(left, right)
+    
+    return dp[ind1][ind2]
     
     
 str1 = 'brute'
@@ -18,7 +46,11 @@ str2 = 'groot'
 ind1 = len(str1)
 ind2 = len(str2)
 
-out = fun(str1, str2, ind1-1, ind2-1)
+dp = [[-1 for _ in range(ind2+1)] for _ in range(ind1+1)]
+
+out = fun(str1, str2, ind1-1, ind2-1, dp)
 print(ind1+ ind2 - out)
+
+display(str1, str2, dp)
 
 
