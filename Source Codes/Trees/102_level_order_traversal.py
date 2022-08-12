4b9fd219d65bf04367ac04eff14d4f37956135e9
Biswajit Basak <juelbasak@gmail.com>
2022-08-12 14:04:25 +0530

    Working on Trees

--- Source Codes/Trees/102_level_order_traversal.py
+++ Source Codes/Trees/102_level_order_traversal.py
@@ -1,11 +1,29 @@
 from bst_hardcoded import bst1
 
-# Performed preorder
 def fun(bst1, llist):
-    if bst1 != None:
-        llist.append(bst1.data)
-        fun(bst1.left, llist)
-        fun(bst1.right,llist)
+    queue = [bst1]
+    llist.append([bst1.data])
+    
+    while len(queue) > 0:
+        
+        temp = []
+        l = len(queue)
+        for i in range(l):
+            
+            if queue[i].left != None:
+                queue.append(queue[i].left)
+                temp.append(queue[i].left.data)
+            
+            if queue[i].right != None:
+                queue.append(queue[i].right)
+                temp.append(queue[i].right.data)
+            
+            
+        if len(temp) > 0:
+            llist.append(temp)
+            
+        queue = queue[l:]
+        
         
         
 llist = []
